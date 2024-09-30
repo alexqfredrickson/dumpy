093297bdf026f32b7af973627595bacb71f8ac06
Alex <alex.q.fredrickson@gmail.com>
2024-09-30 11:45:27 -0400

    added util to recreate a dumpyfile from a sqlite db

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import json
+import sqlite3
+import datetime
 from models import Question
 
 
@@ -72,3 +74,68 @@ class DumpyfileUtils:
             )
 
             return j
+
+    @staticmethod
+    def generate_dumpyfile_from_database(database_path, output_path):
+
+        questions, answers, metadata, conn = None, None, None, None
+
+        try:
+            conn = sqlite3.connect(database_path)
+            c = conn.cursor()
+
+            c.execute("SELECT * FROM metadata")
+            metadata = c.fetchone()
+
+            c.execute("SELECT * FROM questions")
+            questions = c.fetchall()
+
+            c.execute("SELECT * FROM answers")
+            answers = c.fetchall()
+
+            conn.close()
+
+        except sqlite3.Error as e:
+            print(e)
+
+        finally:
+            if conn:
+                conn.close()
+
+        description = metadata[0]
+        shuffle_answers = metadata[1]
+        shuffle_questions_by_weight = metadata[2]
+
+        dumpyfile = {
+            "metadata": {
+                "description": description,
+                "shuffle_answers": shuffle_answers,
+                "shuffle_questions_by_weight": shuffle_questions_by_weight,
+                # "database_created_time": datetime.datetime.strftime(datetime.datetime.now())
+            },
+            "questions": []
+        }
+
+        for q in questions:
+            new_question = {
+                    "text": q[1],
+                    "portmortem": q[2],
+                    "attempted_count": q[3],
+                    "correct_count": q[4],
+                    "enabled": q[5],
+                    "answers": []
+                }
+
+            for a in answers:
+                if a[1] == q[0]:
+                    new_question["answers"].append(
+                        {
+                            "text": a[2],
+                            "is_correct": True if a[3] == 1 else False
+                        }
+                    )
+
+            dumpyfile["questions"].append(new_question)
+
+        with open(output_path, "w+") as output_file:
+            output_file.write(json.dumps(dumpyfile, indent=4))
